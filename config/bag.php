dbe1cd02f5d70a740c9cb7c60b016f1749fedbde
Norquensi Ricardo <norquensir@outlook.com>
2024-07-01 17:12:52 +0200

    BagHelper instead of routes

--- config/bag.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-return [
-    'routes' => [
-        'middleware' => ['web'],
-    ]
-];
---
+++ src/Helpers/BagHelper.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Norquensir\Bag\Helpers;
+
+class BagHelper
+{
+    public function findByPostal($postal, )
+    {
+
+    }
+}
