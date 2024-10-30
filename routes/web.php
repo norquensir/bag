01833752ea289f31a28f6a7eb5e1c519c233cfce
Norquensi Ricardo <norquensir@outlook.com>
2024-10-30 01:48:05 +0100

    Fix routes

--- routes/web.php
+++ routes/web.php
@@ -5,7 +5,7 @@ use App\Http\Controllers\PostalController;
 use App\Http\Controllers\SearchController;
 use Illuminate\Support\Facades\Route;
 
-Route::prefix('bag')->group(function () {
+Route::middleware('web')->group(function () {
     Route::get('/', PostalController::class);
     Route::get('/id/{identifier}', IdentifierController::class);
     Route::get('/s', SearchController::class);
