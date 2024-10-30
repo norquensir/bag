adf20584d5d07e6186489e7ce9d96b4f83b2c16f
Norquensi Ricardo <norquensir@outlook.com>
2024-10-30 01:41:25 +0100

    Enable routes

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,12 @@
 <?php
 
+use App\Http\Controllers\IdentifierController;
+use App\Http\Controllers\PostalController;
+use App\Http\Controllers\SearchController;
 use Illuminate\Support\Facades\Route;
 
-//Route::prefix('bag')->group(function () {
-//    Route::get('/', PostalController::class);
-//    Route::get('/id/{identifier}', IdentifierController::class);
-//    Route::get('/s', SearchController::class);
-//});
+Route::prefix('bag')->group(function () {
+    Route::get('/', PostalController::class);
+    Route::get('/id/{identifier}', IdentifierController::class);
+    Route::get('/s', SearchController::class);
+});
